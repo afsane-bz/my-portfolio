b1a9c4b6a3f77fa5fe36cb752f3ae9b43eb13618
afsane Bh <afsanebh87@gmail.com>
2020-11-23 12:35:48 +0330

    "updates1"

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menuIcon = document.querySelector(".burger");
 const navbar = document.querySelector(".navbar");
+const nvabaractive=document.querySelector(".nav-active")
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
+  nav-avtive.classList.toggle("nav-active")
 });
